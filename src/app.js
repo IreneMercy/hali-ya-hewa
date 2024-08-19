b3c14d63732f2e1064773039f38581ffc8b306c7
Irene Mercy <36657015+IreneMercy@users.noreply.github.com>
2024-08-19 23:22:26 +0300

    Linked search functionality with api

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector(".search-city");
+function updateWeatherDetails(response) {
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°";
   let currentCity = document.querySelector(".inputcity");
-  let cityValue = city.value;
-  if (cityValue.length > 4) {
-    currentCity.innerHTML = cityValue;
-  } else {
-    alert("Please input a valid city name🙏");
-  }
+  currentCity.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "b8a2a2of6ef0b2dad8384b77f37302t6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherDetails);
 }
 
-let searchButton = document.querySelector(".submit-search");
-searchButton.addEventListener("click", searchCity);
+function inputValue(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city");
+  searchCity(city.value);
+}
+
+let searchButton = document.querySelector("#submit-search");
+searchButton.addEventListener("click", inputValue);
